0368f488300998c9a92c0dd49e6b02b0dbf3d518
rayhanasyraff <53102111+rayhanasyraff@users.noreply.github.com>
2026-03-19 07:12:42 +0800

    Fix api docs route

--- apps/api/config/l5-swagger.php
+++ apps/api/config/l5-swagger.php
@@ -1,85 +1,46 @@
 <?php
 
 return [
-    'default' => 'default',
-    'documentations' => [
-        'default' => [
-            'api' => [
-                'title' => 'PoesyLiang Blog API',
-            ],
-            'routes' => [
-                'api' => 'documentation',
-            ],
-            'paths' => [
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
-                'docs_json'         => 'api-docs.json',
-                'docs_yaml'         => 'api-docs.yaml',
-                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
-                'annotations'       => [
-                    base_path('app'),
-                ],
-            ],
+    'api' => [
+        'title' => 'PoesyLiang Blog API',
+    ],
+
+    'routes' => [
+        'api'             => 'documentation',
+        'docs'            => 'docs',
+        'oauth2_callback' => 'api/oauth2-callback',
+        'middleware' => [
+            'api'             => [],
+            'asset'           => [],
+            'docs'            => [],
+            'oauth2_callback' => [],
         ],
     ],
-    'defaults' => [
-        'routes' => [
-            'docs'        => 'docs',
-            'oauth2_callback' => 'api/oauth2-callback',
-            'middleware' => [
-                'api'             => [],
-                'asset'           => [],
-                'docs'            => [],
-                'oauth2_callback' => [],
-            ],
-            'group_options' => [],
+
+    'paths' => [
+        'docs'       => storage_path('api-docs'),
+        'docs_json'  => 'api-docs.json',
+        'docs_yaml'  => 'api-docs.yaml',
+        'annotations' => [
+            base_path('app'),
         ],
-        'paths' => [
-            'docs'     => storage_path('api-docs'),
-            'views'    => base_path('resources/views/vendor/l5-swagger'),
-            'base'     => env('L5_SWAGGER_BASE_PATH', null),
-            'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
-            'excludes' => [],
-        ],
-        'scanOptions' => [
-            'analyser'      => null,
-            'analysis'      => null,
-            'processors'    => [],
-            'pattern'       => null,
-            'exclude'       => [],
-        ],
-        'securityDefinitions' => [
-            'securitySchemes' => [
-                'bearerAuth' => [
-                    'type'         => 'http',
-                    'scheme'       => 'bearer',
-                    'bearerFormat' => 'JWT',
-                ],
-            ],
-            'security' => [
-                ['bearerAuth' => []],
-            ],
-        ],
-        'generate_always'        => env('L5_SWAGGER_GENERATE_ALWAYS', false),
-        'generate_yaml_copy'     => env('L5_SWAGGER_GENERATE_YAML_COPY', false),
-        'proxy'                  => false,
-        'additional_config_url'  => null,
-        'operations_sort'        => env('L5_SWAGGER_OPERATIONS_SORT', null),
-        'validator_url'          => null,
-        'ui'                     => [
-            'display' => [
-                'doc_expansion'     => env('L5_SWAGGER_UI_DOC_EXPANSION', 'none'),
-                'filter'            => env('L5_SWAGGER_UI_FILTERS', true),
-                'show_request_headers' => false,
-            ],
-            'authorization' => [
-                'persist_authorization' => env('L5_SWAGGER_UI_PERSIST_AUTHORIZATION', true),
-                'oauth2' => [
-                    'use_pkce_with_authorization_code_grant' => false,
-                ],
-            ],
-        ],
-        'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost'),
-        ],
+        'views'      => base_path('resources/views/vendor/l5-swagger'),
+        'base'       => env('L5_SWAGGER_BASE_PATH', null),
+        'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
+        'excludes'   => [],
+    ],
+
+    'security' => [],
+
+    'generate_always'    => env('L5_SWAGGER_GENERATE_ALWAYS', false),
+    'generate_yaml_copy' => env('L5_SWAGGER_GENERATE_YAML_COPY', false),
+    'swagger_version'    => env('SWAGGER_VERSION', '3.0'),
+    'proxy'              => false,
+    'additional_config_url' => null,
+    'operations_sort'    => env('L5_SWAGGER_OPERATIONS_SORT', null),
+    'validator_url'      => null,
+
+    'constants' => [
+        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://poesyliang.net/api'),
     ],
 ];
