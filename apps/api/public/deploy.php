1344effd4fea5cd749ceb69ed9f88eba4bafe775
rayhanasyraff <53102111+rayhanasyraff@users.noreply.github.com>
2026-03-19 06:13:26 +0800

    Move deploy php to public

--- apps/api/public/deploy.php
+++ apps/api/public/deploy.php
@@ -1,12 +1,11 @@
 <?php
-// Security check
 if (!isset($_GET['key']) || $_GET['key'] !== 'xv3DEXuHsHn969ducYT3') {
     http_response_code(403);
     die('Forbidden');
 }
 
-$zipPath = __DIR__ . '/vendor.zip';
-$extractTo = __DIR__;
+$zipPath   = __DIR__ . '/../vendor.zip';
+$extractTo = __DIR__ . '/..';
 
 $zip = new ZipArchive;
 if ($zip->open($zipPath) === TRUE) {
